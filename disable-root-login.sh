6c25741f382b0e6c80d4493e4b201e34cf4e980d
rakshana-cloudsec <rakshana.tech.au@gmail.com>
2025-07-20 10:19:10 +1000

    Add disable-root-login.sh script

--- disable-root-login.sh
+++ disable-root-login.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Script to disable root login via SSH
+
+CONFIG_FILE="/etc/ssh/sshd_config"
+
+echo "Before change:"
+grep "^PermitRootLogin" $CONFIG_FILE || echo "PermitRootLogin not set"
+
+sudo sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' $CONFIG_FILE
+
+echo "After change:"
+grep "^PermitRootLogin" $CONFIG_FILE
+echo "Root login via SSH has been disabled."
 
