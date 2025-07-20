a3ec38b67c4f909a0b175e3ccd3e8df982e781db
Rakshana Kannaya Muralidharan <rakshana.tech.au@gmail.com>
2025-07-20 10:03:26 +1000

    Update disable-root-login.sh

--- disable-root-login.sh
+++ disable-root-login.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-# Disable root login over SSH
+# Disable root login over SSH with feedback
+
+echo "Checking current PermitRootLogin setting..."
+current_setting=$(grep "^PermitRootLogin" /etc/ssh/sshd_config)
+
+echo "Before change: $current_setting"
 
 sudo sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+
+echo "Restarting SSH service..."
 sudo systemctl restart sshd
+
+echo "After change:"
+grep "^PermitRootLogin" /etc/ssh/sshd_config
 
 echo "Root SSH login disabled successfully."
